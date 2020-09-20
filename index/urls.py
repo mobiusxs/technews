97bf3a27f6e72102a3a92e673e8a9d5d1b5ca884
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2020-09-20 05:01:32 -0500

    Create submit view

--- index/urls.py
+++ index/urls.py
@@ -6,6 +6,7 @@ app_name = 'index'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('submit/', views.SubmitView.as_view(), name='submit'),
     path('thread/<int:pk>', views.ThreadView.as_view(), name='thread'),
     path('profile/<int:pk>', views.ProfileView.as_view(), name='profile')
 ]
--- index/views.py
+++ index/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse, reverse_lazy, resolve, re_path
 
 from .models import Link, Profile
 
@@ -16,3 +17,12 @@ class ThreadView(DetailView):
 class ProfileView(DetailView):
     template_name = 'index/profile.html'
     model = Profile
+
+
+class SubmitView(CreateView):
+    template_name = 'index/submit.html'
+    model = Link
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse('index:thread', args=[self.object.id])
