127c0bacc885465c6f63693a6cecd102b54fc688
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2020-09-20 04:32:44 -0500

    Create initial models

--- index/admin.py
+++ index/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Link, Comment, Profile
+
+admin.site.register(Profile)
+admin.site.register(Link)
+admin.site.register(Comment)
