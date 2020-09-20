dbd8bfec4f41ae673f5587a200141e316a61fddb
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2020-09-20 04:33:26 -0500

    Create thread and profile views

--- index/urls.py
+++ index/urls.py
@@ -6,4 +6,6 @@ app_name = 'index'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('thread/<int:pk>', views.ThreadView.as_view(), name='thread'),
+    path('profile/<int:pk>', views.ProfileView.as_view(), name='profile')
 ]
--- index/views.py
+++ index/views.py
@@ -1,5 +1,18 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
+
+from .models import Link, Profile
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = 'index/index.html'
+    model = Link
+
+
+class ThreadView(DetailView):
+    template_name = 'index/thread.html'
+    model = Link
+
+
+class ProfileView(DetailView):
+    template_name = 'index/profile.html'
+    model = Profile
