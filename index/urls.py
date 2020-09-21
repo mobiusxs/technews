1b87a502dd3031f7603a9767b9e53afafce932ef
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2020-09-21 02:06:27 -0500

    rename views

--- index/urls.py
+++ index/urls.py
@@ -5,11 +5,12 @@ from . import views
 app_name = 'index'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.LinkListView.as_view(), name='index'),
+    path('links/', views.LinkListView.as_view(), name='links'),
+    path('link/<int:pk>', views.LinkDetailView.as_view(), name='link'),
+
     path('submit/', views.SubmitView.as_view(), name='submit'),
     path('comment/', views.CommentFormView.as_view(), name='comment'),
-    path('newlinks/', views.NewLinkListView.as_view(), name='newlinks'),
     path('newcomments/', views.NewCommentListView.as_view(), name='newcomments'),
-    path('thread/<int:pk>', views.ThreadView.as_view(), name='thread'),
     path('profile/<str:username>', views.ProfileView.as_view(), name='profile'),
 ]
