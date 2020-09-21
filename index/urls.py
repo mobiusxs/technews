5610b98a66a3e745e938e21521a86cc51246fd04
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2020-09-21 02:27:45 -0500

    rename view, reorder

--- index/urls.py
+++ index/urls.py
@@ -8,9 +8,10 @@ urlpatterns = [
     path('', views.LinkListView.as_view(), name='index'),
     path('links/', views.LinkListView.as_view(), name='links'),
     path('link/<int:pk>', views.LinkDetailView.as_view(), name='link'),
-
     path('submit/', views.SubmitView.as_view(), name='submit'),
+
     path('comment/', views.CommentFormView.as_view(), name='comment'),
-    path('newcomments/', views.NewCommentListView.as_view(), name='newcomments'),
+    path('comments/', views.CommentListView.as_view(), name='comments'),
+
     path('profile/<str:username>', views.ProfileView.as_view(), name='profile'),
 ]
