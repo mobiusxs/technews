6ea133cfd8ebea1c85bd94472bb4440e882a17be
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2020-10-01 08:57:29 -0500

    create model manager to include annotations on threads for comments and karma

--- thread/models.py
+++ thread/models.py
@@ -1,6 +1,38 @@
 from django.db import models
+from django.db.models.expressions import RawSQL
 
 from user.models import User
+
+
+class ThreadManager(models.Manager):
+    def get_queryset(self):
+        """Combining multiple aggregations with annotate() will yield the
+        wrong results because joins are used instead of subqueries:
+
+        https://docs.djangoproject.com/en/3.1/topics/db/aggregation/#combining-multiple-aggregations
+
+        https://docs.djangoproject.com/en/3.1/ref/models/expressions/#raw-sql-expressions
+
+        Ultimately, the needed query is:
+
+            SELECT
+                *,
+                (SELECT Sum(vote_threadvote.value) FROM vote_threadvote WHERE thread_thread.id = vote_threadvote.thread_id) AS karma,
+                (SELECT Count(comment_comment.id) FROM comment_comment WHERE thread_thread.id = comment_comment.thread_id) AS comment_count
+            FROM
+                thread_thread
+            GROUP BY
+                thread_thread.id
+
+        In order to get to this, the subqueries must be completely isolated because `subquery=True` doesn't work.
+        Huge deficiency in Django's ORM.
+        """
+
+        qs = super().get_queryset()
+        return qs.annotate(
+            comment_count=RawSQL("""SELECT Count(comment_comment.id) FROM comment_comment WHERE thread_thread.id = comment_comment.thread_id""", ()),
+            karma=RawSQL("""SELECT Sum(vote_threadvote.value) FROM vote_threadvote WHERE thread_thread.id = vote_threadvote.thread_id""", ()),
+        )
 
 
 class Thread(models.Model):
@@ -12,6 +44,7 @@ class Thread(models.Model):
     text = models.CharField(max_length=200)
     datetime = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    objects = ThreadManager()
 
     class Meta:
         ordering = ['-datetime']
