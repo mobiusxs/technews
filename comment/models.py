fd66fecb45902d2314d44a3f88bbcc92584f3499
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2020-10-01 08:58:12 -0500

    create manager to add karma to comments

--- comment/models.py
+++ comment/models.py
@@ -4,6 +4,11 @@ from thread.models import Thread
 from user.models import User
 
 
+class CommentManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().annotate(karma=models.Sum('commentvote__value'))
+
+
 class Comment(models.Model):
     """Simple temporary Comment model.
     Supports only top-level comments for now.
@@ -15,6 +20,7 @@ class Comment(models.Model):
     text = models.TextField(max_length=1000)
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    objects = CommentManager()
 
     def __str__(self):
         return f'[{self.user.username}] {self.datetime}'
