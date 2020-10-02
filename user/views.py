9d55e52d2c86a3ed15853b63bf4fbb5c955f8745
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2020-10-02 04:03:29 -0500

    use karma totals on the model rather than in context data

--- user/views.py
+++ user/views.py
@@ -3,7 +3,6 @@ from django.views.generic import DetailView
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.db.models import Sum
 
 from .models import User
 
@@ -17,14 +16,6 @@ class UserDetailView(DetailView):
 
         username = self.kwargs.get('username')
         return get_object_or_404(User, username=username)
-
-    def get_context_data(self, **kwargs):
-        """Enable displaying comment and thread karma totals on user profile view"""
-
-        context = super().get_context_data(**kwargs)
-        context['comment_karma'] = User.objects.filter(id=self.object.id).aggregate(comment_karma=Sum('comment__commentvote__value'))['comment_karma']
-        context['thread_karma'] = User.objects.filter(id=self.object.id).aggregate(thread_karma=Sum('thread__threadvote__value'))['thread_karma']
-        return context
 
 
 @login_required
