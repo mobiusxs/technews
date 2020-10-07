2c2bb64b5418d69f675d9b46d0d6eb6b71437552
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2020-10-07 17:19:02 -0500

    change variable names, document code

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,11 @@
+/**
+ * Send the vote to the server as a POST request
+ *
+ * Example usage:
+ *      onclick="vote(this)"
+ *
+ * @param  {Element} btn The up or down vote icon element
+ */
 function vote(btn){
     const csrf_token = btn.dataset.csrf;
     const [type, id, vote] = btn.id.split('-');
@@ -10,11 +18,19 @@ function vote(btn){
 }
 
 
-function vote_server(type, id, vote, csrf_token){
+/**
+ * Send the vote to the server as a POST request
+ *
+ * @param  {String} type Can be one of [thread, comment]
+ * @param  {String} id The id for the content being voted on
+ * @param  {String} vote Can be one of [up, down]
+ * @param  {String} csrf CSRF token passed into the template
+ */
+function vote_server(type, id, vote, csrf){
     let xhr = new XMLHttpRequest();
     xhr.open("POST", `/vote/${type}/`, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('X-CSRFToken', csrf_token);
+    xhr.setRequestHeader('X-CSRFToken', csrf);
 
     let value = 1;
     if (vote === "down"){
@@ -29,49 +45,74 @@ function vote_server(type, id, vote, csrf_token){
 }
 
 
+/**
+ * Update the client to reflect voting action.
+ * Increment/decrement the score and change the icon color as necessary.
+ * Icon color is handled by adding/removing a CSS class.
+ * State is stored as a data attribute on the element
+ * which displays the karma value.
+ * There are 3 possible states [null, up, down]
+ * and 6 possible state transitions.
+ *
+ * @param  {Element} karma_element The element displaying the karma value
+ * @param  {Element} upvote_element The upvote icon
+ * @param  {Element} downvote_element The downvote icon
+ * @param  {String} vote Can be one of [up, down]
+ */
 function vote_client(karma_element, upvote_element, downvote_element, vote){
     const start_state = karma_element.dataset.state;
     const [k, _] = karma_element.innerText.split(' ');
     let karma_value = parseInt(k);
     const classname = 'voted';
+    let end_state = start_state;
 
+    /**
+     * For each possible state:
+     * - add remove color class
+     * - get new karma value
+     * - get new state
+     */
     if (start_state === "null"){
         if (vote === "up"){
             upvote_element.classList.add(classname);
             karma_value += 1;
-            karma_element.dataset.state = "up";
+            end_state = "up";
         } else {
             downvote_element.classList.add(classname);
             karma_value -= 1;
-            karma_element.dataset.state = "down";
+            end_state = "down";
         }
     } else if (start_state === "up"){
         if (vote === "up"){
             upvote_element.classList.remove(classname);
             karma_value -= 1;
-            karma_element.dataset.state = "null";
+            end_state = "null";
         } else {
             upvote_element.classList.remove(classname);
             downvote_element.classList.add(classname);
             karma_value -= 2;
-            karma_element.dataset.state = "down";
+            end_state = "down";
         }
     } else if (start_state === "down"){
         if (vote === "up"){
             upvote_element.classList.add(classname);
             downvote_element.classList.remove(classname);
             karma_value += 2;
-            karma_element.dataset.state = "up";
+            end_state = "up";
         } else {
             downvote_element.classList.remove(classname);
             karma_value += 1;
-            karma_element.dataset.state = "null";
+            end_state = "null";
         }
     }
 
+    // handle plurality of "point" based on karma value
     let points = " points";
     if (karma_value === 1 || karma_value === -1){
         points = " point";
     }
+
+    // Update state and karma
+    karma_element.dataset.state = end_state;
     karma_element.innerText = karma_value + points;
 }
